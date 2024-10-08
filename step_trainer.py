d1ed47744c028594b1323daaf364fd936c7d6bb9
BekhtiWissal <135755494+BekhtiWissal@users.noreply.github.com>
2024-10-08 14:31:40 +0100

    Add files via upload

--- step_trainer.py
+++ step_trainer.py
@@ -1,4 +1,4 @@
-from agents.qmix_full.agent import Agent
+from agents.ped_dqn.agent import Agent
 from agents.base_trainer import Trainer as BaseTrainer
 from agents.base_trainer import stringify
 from agents.simple_agent import RunningAgent as NonLearningAgent
@@ -6,17 +6,18 @@ import numpy as np
 import tensorflow as tf
 import config
 from datetime import datetime
-
-np.set_printoptions(precision=2)
-
+from envs.scenarios.battery_endless import Scenario
+from envs.environment import MultiAgentEnv
+np.set_printoptions(precision=3)
+from envs.grid_core import World
 FLAGS = config.flags.FLAGS
 minibatch_size = FLAGS.minibatch_size
 n_predator = FLAGS.n_predator
 n_prey = FLAGS.n_prey
 test_interval = FLAGS.test_interval
 train_interval = FLAGS.train_interval
-map_size = FLAGS.map_size
-
+quota = FLAGS.max_quota
+ 
 
 
 class Trainer(BaseTrainer):
@@ -24,19 +25,18 @@ class Trainer(BaseTrainer):
         self.env = environment
         self.logger = logger
         self.n_agents = n_predator + n_prey
+        self.scenario = Scenario()
+        
+        self.encounter_all, self.encounter_step = self.scenario.encounters_count()[0], self.scenario.encounters_count()[1]
 
         self.sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(allow_growth=True)))
 
         self._agent_profile = self.env.get_agent_profile()
         agent_precedence = self.env.agent_precedence
-
-        self.predator_singleton = Agent(
-            act_space=self._agent_profile["predator"]["act_spc"],
-            obs_space=self._agent_profile["predator"]["obs_dim"],
-            sess=self.sess,
-            n_agents=n_predator,
-            name="predator"
-        )
+        self.predator_singleton = Agent(act_space=self._agent_profile["predator"]["act_spc"],
+                                        obs_space=self._agent_profile["predator"]["obs_dim"],
+                                        sess=self.sess, n_agents=n_predator,
+                                        name="predator")
 
         self.agents = []
         for i, atype in enumerate(agent_precedence):
@@ -47,7 +47,6 @@ class Trainer(BaseTrainer):
 
             self.agents.append(agent)
 
-        # Initialize tf variables
         self.sess.run(tf.compat.v1.global_variables_initializer())
         self.saver = tf.compat.v1.train.Saver()
 
@@ -59,6 +58,19 @@ class Trainer(BaseTrainer):
         else:
             self.predator_singleton.sync_target()
 
+    def density_within_observable_space(self, agent_positions, observable_range=(7, 7)):
+        densities = []
+        for x, y in agent_positions:
+            count = 0
+            x_min, x_max = x - observable_range[0] // 2, x + observable_range[0] // 2
+            y_min, y_max = y - observable_range[1] // 2, y + observable_range[1] // 2
+            for nx, ny in agent_positions:
+                if x_min <= nx <= x_max and y_min <= ny <= y_max and (nx, ny) != (x, y):
+                    count += 1
+            densities.append(count)
+        return densities
+    
+    # Function to get neighbors in the 7x7 observable space
     def get_neighbors_within_observable_space(self, position, predator_positions, observable_range=(7, 7)):
         neighbors = []
         x, y = position
@@ -69,15 +81,40 @@ class Trainer(BaseTrainer):
                 neighbors.append((i, nx, ny))
         return neighbors
 
-    def get_closest_agents(self, position, predator_positions, k=6):
+    # Function to get the top 3 neighbors with the highest reputations
+    def get_top_3_neighbors_with_highest_reputations(self, predator_positions, reputations, observable_range=(7, 7)):
+        top_neighbors = {}
+        for agent_idx, position in enumerate(predator_positions):
+            neighbors = self.get_neighbors_within_observable_space(position, predator_positions, observable_range)
+            neighbor_reputations = []
+            for neighbor_idx, nx, ny in neighbors:
+                neighbor_reputations.append((neighbor_idx, reputations[neighbor_idx]))
+            # Sort neighbors by reputation and select the top 3
+            neighbor_reputations.sort(key=lambda x: x[1], reverse=True)
+            top_neighbors[agent_idx] = neighbor_reputations[:4]
+        return top_neighbors
+    
+
+    def get_incentives(self, info_n):
+        inc_n = self.predator_singleton.incentivize_multi(info_n)
+        inc_n = inc_n.tolist()
+
+        for agent in self.agents[n_predator:]:
+            inc_n.append(0)
+
+        return inc_n
+    
+    def distance(self, agent_idx, closest_neighbors): 
+        agent_position = np.array(self.env.get_predator_positions()[agent_idx])  # Convert to NumPy array
         distances = []
-        for i, pos in enumerate(predator_positions):
-            if pos != position:
-                dist = np.sqrt((pos[0] - position[0]) ** 2 + (pos[1] - position[1]) ** 2)
-                distances.append((i, dist))
-        distances.sort(key=lambda x: x[1])
-        closest_indices = [i for i, _ in distances[:k]]
-        return closest_indices
+        
+        for neighbor_idx in closest_neighbors:
+            neighbor_position = np.array(self.env.get_predator_positions()[neighbor_idx])  # Convert to NumPy array
+            dist = np.linalg.norm(agent_position - neighbor_position)  # Now this works
+            distances.append(dist)
+        
+        return distances
+
 
 
     def learn(self, max_global_steps, max_step_per_ep):
@@ -93,49 +130,51 @@ class Trainer(BaseTrainer):
         steps_before_train = min(FLAGS.minibatch_size*4, FLAGS.rb_capacity)
 
         tds = []
+        mtds = []
         ep = 0
         global_step = 0
-        
         while global_step < max_global_steps:
             ep += 1
             obs_n = self.env.reset()
-            ax, ay = self.env.get_coordinates(range(self.n_agents))
-            coords = np.asarray([ax, ay], dtype=np.float32).reshape(-1) / (map_size - 1)
+            self.predator_singleton.reset()
 
             for step in range(max_step_per_ep):
-                global_step += 1
+                global_step += 1                
+
+                # Get the action using epsilon-greedy policy
                 act_n = self.get_actions(obs_n, epsilon)
+                #print("act_n", act_n[:n_predator])
 
-                predator_positions = self.env.get_predator_positions()
-                closest_indices_lists = []
-                for predator in range(0, n_predator):
-                    closest_indices = self.get_closest_agents(predator_positions[predator], predator_positions)
-                    closest_indices_lists.append(closest_indices)
-
+                # Do the action and update observation
                 obs_n_next, reward_n, done_n, _ = self.env.step(act_n)
                 done = done_n[:n_predator].all()
                 done_n[:n_predator] = done
-                ax, ay = self.env.get_coordinates(range(self.n_agents))
-                next_coords = np.asarray([ax, ay], dtype=np.float32).reshape(-1) / (map_size - 1)
+                
+                predator_positions = self.env.get_predator_positions()
 
-                next_predator_positions = self.env.get_predator_positions()
-                next_closest_indices_lists = []
-                for predator in range(0, n_predator):
-                    next_closest_indices = self.get_closest_agents(next_predator_positions[predator], next_predator_positions)
-                    next_closest_indices_lists.append(next_closest_indices)
+                for agent in range(n_predator):
+                    closest_neighbors = self.get_neighbors_within_observable_space(predator_positions[agent], predator_positions)
+                    
+                    closest_neighbor_indices = [neighbor[0] for neighbor in closest_neighbors]
+
+                    distances = self.distance(agent, closest_neighbor_indices)
+
+                    if len(distances) > 0:  
+                        if distances[0] > 2: 
+                            if len(distances) > 1 and distances[1] > 2:  
+                                reward_n[agent] -= 100  
                 
-                exp = [obs_n[:n_predator], act_n[:n_predator], reward_n[:n_predator],
-                    obs_n_next[:n_predator], done_n[:n_predator], coords, next_coords, closest_indices_lists, next_closest_indices_lists]
+                transition = [obs_n[:n_predator], act_n[:n_predator], 
+                    reward_n[:n_predator], obs_n_next[:n_predator], done_n[:n_predator]]
                 
-                
-                self.predator_singleton.add_to_memory(exp)
+
+                self.predator_singleton.add_to_memory(transition)
 
                 if global_step > steps_before_train and global_step % train_interval == 0:
-                    td = self.predator_singleton.train()
+                    td = self.predator_singleton.train(use_rx=False)
                     tds.append(td)
 
                 if global_step % test_interval == 0:
-                    
                     mean_steps, mean_b_reward, mean_captured, success_rate, rem_bat = self.test(25, max_step_per_ep)
                 
                     time_diff = datetime.now() - start_time
@@ -153,13 +192,14 @@ class Trainer(BaseTrainer):
                     self.logger.info("%d\tsuccs\t%s" %(global_step, stringify(success_rate[:n_predator], "\t")))
                     self.logger.info("%d\tbttry\t%s" %(global_step, stringify(rem_bat, "\t")))
 
-                    td = np.asarray(tds).mean()
-                    self.logger.info("%d\ttd_er\t%0.2f" %(global_step, td))
+                    td = np.asarray(tds).mean(axis=1)
+                    self.logger.info("%d\ttd_er\t%s" %(global_step, stringify(td[:n_predator], "\t")))
                     tds = []
 
                 if done or global_step == max_global_steps: 
                     break
 
                 obs_n = obs_n_next
-                coords = next_coords
                 epsilon = max(epsilon_min, epsilon - epsilon_dec)
+
+    
